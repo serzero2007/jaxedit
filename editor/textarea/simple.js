9dfa9ff0e6c7f1aee19e8aabf5523c60813d1769
serzero2007 <serzero2007@gmail.com>
2015-05-25 22:40:07 +0300

    Many fixes and improvements
    UI code refactoring.
    Buttons deleted, Codemirror finally added as it meant to be.
    Dialogs UI code deleted.
    SkyDrive code deleted.
    Much of removed functionality is to be restored in future.

--- editor/textarea/simple.js
+++ editor/textarea/simple.js
@@ -8,218 +8,111 @@
  * Release: http://code.google.com/p/jaxedit/
  */
 
-jaxedit.doChange = function(event) {
-  var ev = event ? event : window.event; // standard or ie
-  var agent = $.agent;
-  var childs = jaxedit.childs,
-      codearea = childs.codearea,
-      lbot = childs.lbot,
-      showarea = childs.showarea;
-  var data = jaxedit.textdata;
-  var oldtextvalue = data.oldtextvalue,
-      oldtextsize  = data.oldetextsize,
-      oldselstart  = data.oldselstart,
-      oldselend    = data.oldselend,
-      oldseltext   = data.oldseltext,
-      newtextvalue = data.newtextvalue,
-      newtextsize  = data.newtextsize,
-      newselstart  = data.newselstart,
-      newselend    = data.newselend,
-      newseltext   = data.newseltext;
+jaxedit.doChange = function () {
+	var agent = $.agent;
+	var childs = jaxedit.childs,
+		codearea = childs.codearea,
+		lbot = childs.lbot,
+		showarea = childs.showarea;
+	var data = jaxedit.textdata;
+	var oldtextvalue = data.oldtextvalue,
+		oldtextsize = data.oldetextsize,
+		oldselstart = data.oldselstart,
+		oldselend = data.oldselend,
+		oldseltext = data.oldseltext,
+		newtextvalue = data.newtextvalue,
+		newtextsize = data.newtextsize,
+		newselstart = data.newselstart,
+		newselend = data.newselend,
+		newseltext = data.newseltext;
 
-  oldselstart  = newselstart;
-  oldselend    = newselend;
-  oldtextvalue = newtextvalue;
-  oldtextsize  = newtextsize;
-  oldseltext   = newseltext;
+	oldtextvalue = newtextvalue;
+	oldtextsize = newtextsize;
 
-  newtextvalue = codearea.value;
-  newtextsize  = codearea.value.length;
 
-  if (agent.browser == "msie" && agent.version <= 8) {
-    var range1 = document.selection.createRange();
-    var parent = range1.parentElement();
-    if (parent != codearea) {
-      console.log('TextRange: parent element is of tagName ' + parent.tagName);
-      return;
-    }
-    var range2 = range1.duplicate();
-    range2.moveToElementText(codearea);
-    range2.setEndPoint('EndToEnd',range1);
-    newselstart = range2.text.length - range1.text.length;
-    newselend = newselstart + range1.text.length;
-    newseltext = range1.text;
-  } else {
-    newselstart  = codearea.selectionStart;
-    newselend    = codearea.selectionEnd;
-    newseltext   = newtextvalue.substring(newselstart,newselend);
-  }
+	newtextvalue = jaxedit.editor.getValue();
+	newtextsize = newtextvalue.length;
+	
+	var delstart = 0,
+		delend = 0,
+		deltext = "";
+	var insstart = 0,
+		insend = 0,
+		instext = "";
 
-  var omitted = false;
+	delstart = (oldselstart < newselstart) ? oldselstart : newselstart;
+	delend = (oldtextsize - oldselend < newtextsize - newselend) ? oldselend : oldtextsize - newtextsize + newselend;
 
-  if (ev.type == "mousemove" && document.activeElement.id != "codearea") { // codearea unused
-    omitted = true;
-  } else if ( ev.type == "mousemove" && oldselstart == newselstart && oldselend == newselend ) { // codearea in use
-    omitted = true;
-  } else if (ev.type == "focus"){
-    omitted = true;
-  } else if (ev.type == "mousedown"){ // click to clear old selection
-    omitted = true;
-  } else if (ev.type == "mouseup" && newselstart == newselend) {
-    omitted = true;
-  } else if (ev.type == "keydown" && ev.keyCode == 229 && agent.engine == "webkit"){
-    // todo: iuput method in chrome
-    // note: keydown event is also used when pressing a key for some time
-  }
 
-  if (omitted) return;
+	delstart = (delstart - 64 > 0) ? delstart - 64 : 0;
+	delend = (delend + 64 < oldtextsize) ? delend + 64 : oldtextsize;
 
-  var delstart = 0, delend = 0, /*delsize = 0,*/ deltext = "";
-  var insstart = 0, insend = 0, /*inssize = 0,*/ instext = "";
 
-  delstart = (oldselstart < newselstart) ? oldselstart : newselstart;
-  delend = (oldtextsize-oldselend < newtextsize-newselend) ? oldselend : oldtextsize - newtextsize + newselend;
+	// we should always keep these two equalities
+	insstart = delstart;
+	insend = newtextsize - oldtextsize + delend;
+	while (newtextvalue.charAt(delstart) == oldtextvalue.charAt(delstart) && delstart < delend && insstart < insend) {
+		delstart += 1;
+		insstart += 1;
+	}
+	while (newtextvalue.charAt(delend - 1) == oldtextvalue.charAt(delend - 1) && delstart < delend && insstart < insend) {
+		delend -= 1;
+		insend -= 1;
+	}
 
-  if (agent.browser == "firefox" && oldselstart < oldselend && newselstart == newselend) {
-    // fix for draging selection leftward in firefox
-    var tempstart = newselstart - (oldselend - oldselstart);
-    if ( newselend < oldselend && tempstart >= 0 && delstart > tempstart)
-      delstart = tempstart;
-  }
+	deltext = oldtextvalue.substring(delstart, delend);
+	instext = newtextvalue.substring(insstart, insend);
 
-  if (agent.engine == "webkit" || agent.browser == "opera") {
-    // fix for input method in chrome browser
-    // this way is dirty, rewrite it another time
-    delstart = (delstart - 64 > 0) ? delstart - 64 : 0;
-    delend = (delend + 64 < oldtextsize) ? delend + 64 : oldtextsize;
-  }
-  //console.log(delstart,delend);
+	//console.log("textsize:" + newtextsize + "; selection:" + oldselstart + "-" + oldselend + " to " +  newselstart + "-" + newselend + "; change: " + delstart + " to " + delend + "; event:" + ev.type + "; deltext:" + deltext + " ;instext:" + instext);
+	lbot.innerHTML = "size: " + newtextsize + "; selection: " + oldselstart + "-" + oldselend + " to " + newselstart + "-" + newselend;
 
-  // we should always keep these two equalities
-  insstart = delstart;
-  insend = newtextsize - oldtextsize + delend;
+	data.oldtextvalue = oldtextvalue;
+	data.oldetextsize = oldtextsize;
+	data.newtextvalue = newtextvalue;
+	data.newtextsize = newtextsize;
+	data.newselstart = newselstart;
+	data.newselend = newselend;
+	data.newseltext = newseltext;
 
-  while (newtextvalue.charAt(delstart) == oldtextvalue.charAt(delstart) && delstart < delend && insstart < insend){
-    delstart += 1;
-    insstart += 1;
-  }
-  while (newtextvalue.charAt(delend-1) == oldtextvalue.charAt(delend-1) && delstart < delend && insstart < insend){
-    delend -= 1;
-    insend -= 1;
-  }
+	if (window.localStorage) {
+		if (deltext != "" || instext != "") {
+			//IE8 sometimes crashes when writing empty value to a localStorage item
+			if (codearea.value != "") { localStorage.setItem("texcode", codearea.value);} 
+			else {localStorage.removeItem("texcode");}
+		}
+	}
 
-  deltext = oldtextvalue.substring(delstart, delend);
-  instext = newtextvalue.substring(insstart, insend);
-
-  //console.log("textsize:" + newtextsize + "; selection:" + oldselstart + "-" + oldselend + " to " +  newselstart + "-" + newselend + "; change: " + delstart + " to " + delend + "; event:" + ev.type + "; deltext:" + deltext + " ;instext:" + instext);
-  lbot.innerHTML = "size: " + newtextsize + "; selection: " + oldselstart + "-" + oldselend + " to " + newselstart + "-" + newselend;
-
-  data.oldtextvalue = oldtextvalue;
-  data.oldetextsize = oldtextsize;
-  data.oldselstart  = oldselstart;
-  data.oldselend    = oldselend;
-  data.oldseltext   = oldseltext;
-  data.newtextvalue = newtextvalue;
-  data.newtextsize  = newtextsize;
-  data.newselstart  = newselstart;
-  data.newselend    = newselend;
-  data.newseltext   = newseltext;
-
-  if (window.localStorage) {
-    if (deltext != "" || instext != "") {
-      //IE8 sometimes crashes when writing empty value to a localStorage item
-      if (codearea.value != "") {
-        localStorage.setItem("texcode", codearea.value);
-      } else {
-        localStorage.removeItem("texcode");
-      }
-    }
-  }
-
-  jaxedit.setScrollers(newtextsize, delstart, codearea.scrollTop);
-  typejax.updater.putTask(delstart, delend, deltext, instext, newtextsize, showarea);
+	jaxedit.setScrollers(newtextsize, delstart, codearea.scrollTop);
+	typejax.updater.putTask(delstart, delend, deltext, instext, newtextsize, showarea);
 };
 
-jaxedit.addEditor = function() {
-  var codearea = this.childs.codearea;
-
-  this.editor = {
-    getWrapperElement : function() {
-      return codearea;
-    },
-
-    getValue : function() {
-      return codearea.value;
-    },
-
-    setValue : function(value) {
-      codearea.value = value;
-    },
-
-    setReadOnly : function(bool) {
-      codearea.readOnly = bool;
-    },
-
-    getScrollInfo : function() {
-      return {
-        left :         codearea.scrollLeft,
-        top :          codearea.scrollTop,
-        width :        codearea.scrollWidth,
-        height :       codearea.scrollHeight,
-        clientWidth :  codearea.clientWidth,
-        clientHeight : codearea.clientHeight
-      };
-    },
-
-    getViewPort : function() {
-      // return {from, to};
-      alert('unsupported!');
-    },
-
-    scrollTo : function(x, y) {
-      codearea.scrollTop = y;
-    },
-
-    scrollIntoView : function(pos) {
-      // pos = {line, ch}
-      alert('unsupported!');
-    }
-  };
+jaxedit.addEditor = function () {
+	var codearea = this.childs.codearea;
+	var editorCM = CodeMirror.fromTextArea( codearea , {
+			height: "350px",
+		    continuousScanning: 500,
+			lineNumbers: true,
+			lineWrapping: true
+		});
+	
+			
+	this.editor = {
+		getWrapperElement : function () { return editorCM;},
+		getValue : function () { return editorCM.getValue(); },
+		setValue : function (value) { editorCM.setValue(value); },
+		setReadOnly : function (bool) {  },
+		scrollTo : function (x, y) {  },
+		getScrollInfo : function () { return {}; },
+	};
 };
 
-jaxedit.addHandler = function() {
-  var codearea = this.childs.codearea,
-      showarea = this.childs.showarea;
-
-  codearea.onkeydown = this.doChange;
-  //codearea.onkeypress = this.doChange;
-  codearea.onkeyup = this.doChange;
-
-  //codearea.onfocus = this.doChange;
-  //codearea.onblur = this.doChange;
-
-  codearea.onmousedown = this.doChange;
-  codearea.onmousemove = this.doChange;
-  codearea.onmouseup = this.doChange;
-
-  codearea.oncopy = this.doChange;
-  codearea.oncut = this.doChange;
-  codearea.onpaste = this.doChange;
-
-  //codearea.oninput = this.doChange;
-  //codearea.addEventListener("textInput", this.doChange, false);
-  //codearea.addEventListener("textinput", this.doChange, false);
-  //codearea.onchange = this.doChange;
-  //codearea.onpropertychange = this.doChange;
-
-  codearea.onscroll = function () {
-    if (window.localStorage) {
-      localStorage.setItem("scroll", codearea.scrollTop);
-    }
-    jaxedit.doScroll(true);
-  };
-
-  showarea.onscroll = function() {
-    jaxedit.doScroll(false);
-  };
+jaxedit.addHandler = function () {
+	var codearea = this.childs.codearea,
+		showarea = this.childs.showarea;
+	this.editor.getWrapperElement().on("change", function (cm) { jaxedit.doChange(); });
+	showarea.onscroll = function () { jaxedit.doScroll(false); };
+	codearea.onscroll = function () {
+		if (window.localStorage) { localStorage.setItem("scroll", codearea.scrollTop); }
+		jaxedit.doScroll(true);
+	};
 };
